299cbfbbf4653f5049d9feae0668cb31d291fd68
MattiasAxelson <mattias_axelson@hotmail.com>
2017-01-30 13:58:52 +0100

    kNN med lika röster hantering

--- Computer assignment/Supervised/kNN/kNN.m
+++ Computer assignment/Supervised/kNN/kNN.m
@@ -11,65 +11,41 @@ function [ labelsOut ] = kNN(X, k, Xt, Lt)
 % LkNN = kNN(Xt{2}, k, Xt{1}, Lt{1});
 
 labelsOut1 = [];
-labelsOut = [];
 distance = [];
-distanceXY = [];
 kVotes = [];
 
 
-for i = 1: 1: size(X,2)%length(X) 
+for i = 1: 1: size(X,2)
     for j = 1: 1: size(Xt,2);
         distanceXY =  abs(X(:,i) - Xt(:,j));
         distance  = [distance sqrt(distanceXY(1,1)^2 + distanceXY(2,1)^2)];
     end
-    
-     %För flera k   
-    for p = 1: 1: k    
+
+    for p = 1: 1: k
         [M, Index] = min(distance);
-        kVotes = [kVotes Lt(Index,1)];  
-        distance(Index) = [];              
-    end
-    
-    % M Vilken som är mest frekvent
-    % F hur många
-%     [M,F] = mode(kVotes);
+        kVotes = [kVotes Lt(Index,1)];
+        distance(Index) = [];
+    end  
 
- [frequencyOcc,labelNumber]=hist(kVotes,unique(kVotes));
- [mostFreq, indexFreq] = max(frequencyOcc);
-  frequencyOcc(indexFreq) = [];
-  
-%   FIXA FÖR JÄMNA K
-  
-%  if length(frequencyOcc) == 0  
-%      nextMostFreq = 0;
-%  else
-%  nextMostFreq = max(frequencyOcc);
-%  end 
-%      if mostFreq == nextMostFreq
-%       %number is even     
-%       [M, Index] = min(distance);  
-%       labelsOut1 = [labelsOut1 Lt(Index,1)]
-%     else
-%       %number is odd
-%       votedLabel = labelNumber;
-%       labelsOut1 = [labelsOut1 votedLabel]; 
-%     end
- 
-        [M, Index] = min(distance);  
-       labelsOut1 = [labelsOut1 Lt(Index,1)];
-
-        kVotes = [];
-        distance = [];
-    
+        [frequencyOcc,labelNumber]=hist(kVotes,unique(kVotes));
+        [mostFreq, indexFreq] = max(frequencyOcc);
+        frequencyOcc(indexFreq) = [];
+        nextMostFreq = max(frequencyOcc);
+       
+        if mostFreq == nextMostFreq % om oavgjort, tar den kortaste "sträckan"
+              labelsOut1 = [labelsOut1 nextMostFreq];
+        else
+            [labelNumber,F] = mode(kVotes);
+            labelsOut1 = [labelsOut1 labelNumber];
+        end
+        
+    kVotes = [];
+    distance = [];
 end
 
-  labelsOut =  labelsOut1';
-
-% 
-% labelsOut  = zeros(size(X,2),1)
-% classes = unique(Lt);
-% numClasses = length(classes);
-
+labelsOut =  labelsOut1';
 
 end
+
+
 
